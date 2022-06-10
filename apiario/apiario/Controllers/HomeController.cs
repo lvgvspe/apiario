49125c380a1d875591cb9bafee8955f5be14ccb8
Lucas <lucas-camargo@outlook.com>
2022-06-10 12:46:42 -0300

    commit

---
+++ apiario/apiario/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using apiario.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace apiario.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Feature()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+        public IActionResult Testimonial()
+        {
+            return View();
+        }
+    }
+}
---
+++ apiario/apiario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace apiario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
